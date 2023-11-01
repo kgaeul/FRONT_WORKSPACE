ccfea58dd22f84468bda6837dd8271d61a3b1577
kgaeul <gaury0722@naver.com>
2023-11-01 17:45:19 +0900

    html,css,script

--- html/js/blog.js
+++ html/js/blog.js
@@ -1,3 +1,65 @@
+
+  
+ const images = [
+    '../사진/에몽가1.jpg',
+    '../사진/에몽가2.jpg',
+    '../사진/에몽가3.jpg',
+    '../사진/에몽가4.jpg',
+    '../사진/에몽가5.jpg',
+];
+const imagesPerpage =1 ;
+let currentPage = 0; 
+
+const imageContainer = document.querySelector('.img-container');//이미지가 표시될 div공간
+const prevButton = document.getElementById('prevPage');//이전버튼
+const nextButton = document.getElementById('nextPage');//다음버튼
+
+function displayImg(page){
+
+    const startIndex = page * imagesPerpage;
+    const endIndex = startIndex + imagesPerpage;
+    const pageImages = images.slice(startIndex, endIndex);
+
+    imageContainer.innerHTML = '';
+    pageImages.forEach(images => {
+
+        const imgElement = document.createElement('img'); 
+        //<img > 태그에 src 속성을 images(현재 이미지의 파일 경로)로 설정
+        imgElement.src = images;
+         //  태그에 image 클래스를 추가해서 추후 이미지 스타일을 적용할 예정
+        imgElement.classList.add('image');
+        imageContainer.appendChild(imgElement);
+    });
+}
+
+//버튼 업데이트
+function updateButtons() {
+    prevButton.disabled = currentPage ===0;
+    nextButton.disabled = (currentPage + 1) * imagesPerpage >= images.length;
+}
+
+prevButton.addEventListener('click', () => {
+    if (currentPage > 0){
+        currentPage --;
+        displayImg(currentPage);
+        updateButtons();
+        
+    }
+});
+
+nextButton.addEventListener('click', () => {
+
+    if((currentPage + 1) *imagesPerpage < images.length){
+        currentPage++;
+        displayImg(currentPage);
+        updateButtons();
+    }
+})
+
+displayImg(currentPage);
+updateButtons();
+ 
+ 
  //게시글 목록을 가져오고 화면에 나타내기
  function displayPosts() {
     const postlist = document.getElementById('post-list');
@@ -46,6 +108,8 @@
         });
     });
 }
+
+
 
 //addpost 버튼을 클릭했을 때 새창에서 게시글 작성 양식 표시할 수 있도록 설정
 /*
@@ -127,6 +191,8 @@ function logout(){
 }
 
 
+
+
 function submitPassword(){
     var password = document.getElementById("password").value;
     // 정규식을 활용해서 영문자 소/대문자 특수문자 숫자 포함하기 8~32
@@ -158,107 +224,3 @@ function submitPassword(){
 }
 
 
-
-const images=[
-'./pagination/사진/에몽가1.jpg',
-'./pagination/사진/에몽가2.jpg',
-'./pagination/사진/에몽가3.jpg',
-'./pagination/사진/에몽가4.jpg',
-'./pagination/사진/에몽가5.jpg',
-];
-
-const imagePerpage = 2; //한 페이지에 표시될 이미지 수
-let currentPage = 0; //현재 페이지를 나타내는 변수
-//초기값을 0으로 설정하여 첫 번째 페이지를 표시함
-
-const imageContainer = document.querySelector('.img-container');//이미지가 표시될 div 공간
-const prevButton = document.getElementById('prevPage');//이전 버튼
-const nextButton = document.getElementById('nextPage');//다음버튼
-
-
-/*이미지 표시 함수의 시작
-
-displayImg(page) : page는 현재 페이지 번호를 나타냄
-호출 될 때 마다 페이지를 전환하고 해당 페이지의 이미지를 표시하는 데 사용
-함수를 호출하는 공간에서 page값을 전달하면 함수는 해당 페이지에 해당하는 이미지를 표시
-
-displayImg(0) : 첫 번째 페이지에 해당하는 이미지 표시
-displayImg(1) : 두 번째 페이지에 해당하는 이미지 표시
-
-*/
-
-function displayImg(page){
-
-//현재 페이지 번호('page')와 페이지당 이미지 수(imagePerpage)를 곱해서 시작 인덱스를 계산
-// 페이지 내에서 표시할 이미지의 첫 번째 이미지 인덱스를 나타냄
-const startIndex = page * imagePerpage;
-
-//시작 인덱스에 페이지당 이미지 수를 더해서 종료 인덱스를 계산
-//페이지 내에서 표시할 마지막 이미지 다음 인덱스를 나타냄
-const endIndex = startIndex + imagePerpage;
-
-//시작 인덱스에 페이지당 이미지 수를 더해서 종료 인덱스를 계산
-//페이지 내에서 표시할 이미지의 마지막 이미지 다음 인덱스를 표시
-
-//이미지 배열('images')에서 시작 인덱스와 종료 인덱스 사이에 이미지를 추출해서 
-//pageImages 배열에 저장 이 배열은 현재 페이지에 표시될 이미지들을 담고 있음
-const pageImages = images.slice(startIndex, endIndex);
-
-imageContainer.innerHTML=''; //이미지를 표시해 줄 컨테이너
-
-pageImages.forEach(images => {
-    const imgElement = document.createElement('img'); //각 이미지를 표시하기 위한 <img> 태그 요소 생성
-   //<img> 태그에 src 속성을 사진(현재 이미지의 파일 경로)으로 설정
-    imgElement.src = images;
-
-    //<img>태그에 image 클래스를 추가해서 추후 이미지 스타일을 적용할 예정
-    imgElement.classList.add('image'); 
-
-    imageContainer.appendChild(imgElement);
-});
-
-}
-
-function updateButtons(){
-//이전 버튼 활성화 비활성화 시키는 기능을 나타냄 첫 페이지일 경우 이전 버튼을 비활성화 시켜서 첫 페이지에서 이전 버튼 클릭하지 못하게 막음
-prevButton.disabled = currentPage ===0;
-
-/*
-(currentPage+1) * imagePerpage = 다음 페이지에 해당하는 이미지의 첫번째 인덱스
-images.length = <images>배열의 길이로, 페이지를 넘어갈 수 있는 이미지의 전체 수를 나타낸다. 
-
-*/
-
-//다음 버튼에서 마지막 페이지일 경우 이미지가 없을 때 더 이상 다음버튼이 눌리는 것을 방지하기 위해서 다음 버튼 또한 클릭하지 못하게 막음
-nextButton.disabled = (currentPage+1) * imagePerpage >= images.length;
-}
-
-//이전 버튼에 대한 클릭 이벤트
-//사용자가 이전 버튼을 클릭할 때 이 함수가 실행됨
-prevButton.addEventListener('click',()=> {//현재 페이지 번호가 0보다 클 경우에만 실행
-if(currentPage > 0){
-    currentPage --; // 현재 페이지 번호에서 하나를 감소시킴
-    displayImg(currentPage); //새로운 현재 페이지에 해당하는 이미지를 표시
-    // 이전 버튼이 첫 번째 페이지에서 클릭하게 되면 더 이상 눌리지 않게 비활성화 시킴
-    updateButtons(); 
-}
-});
-
-
-nextButton.addEventListener('click', () => {
-//현재 페이지에서 다음 페이지로 이동할 수 있는지 확인
-/*
-currentPage : 현재 페이지 번호
-imagesPerpage : 한 페이지당 표시되는 이미지의 수
-images.length : images 배열의 총 이미지 수
-*/
-if((currentPage + 1) *imagePerpage < images.length){
-//현재 페이지의 번호를 하나 증가시켜 다음 페이지로 이동하게 함
-currentPage++;
-displayImg(currentPage);
-updateButtons();
-}
-});
-
-displayImg(currentPage);
-updateButtons();
